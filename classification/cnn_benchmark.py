7b7708c4fa86bc36b8633bf24f224e4b90b17524
chingheng113 <chingheng113@gmail.com>
2019-03-20 17:15:09 -0400

    back up

--- classification/cnn_benchmark.py
+++ classification/cnn_benchmark.py
@@ -1,43 +1,56 @@
 from keras.engine import Input, Model
-import SimpleITK as sitk
+import numpy as np
 from keras.layers import Dense, Conv3D, MaxPooling3D, Flatten, Input, Activation, BatchNormalization, Dropout
 from keras import backend
 import keras
 import glob, os
 from data import data_util
 
+backend.set_image_data_format('channels_first')
+config = dict()
+config['which_machine'] = 'ct'
+config['image_shape'] = (64, 64, 64)
+if config['which_machine'] == 'ct':
+    config['all_modalities'] = ['ct']
+else:
+    config["all_modalities"] = ['t1', 't1ce', 'flair', 't2']
+config['n_channels'] = len(config["all_modalities"])
+config['input_shape'] = tuple([config['n_channels']] + list(config['image_shape'])) # CHANNEL, WIDTH, HEIGHT, DEPTH
 
 if __name__ == '__main__':
-    img_paths = data_util.get_img_paths('ct')
-    sitk_t1 = sitk.ReadImage(img_paths[0])
-    t1 = sitk.GetArrayFromImage(sitk_t1)
-    # I want overfitting
-    x_train_o = t1.T
-    x_train = x_train_o.reshape(-1, 1, 64, 64, 64)
-    x_test = x_train
+    read_file_path = data_util.write_data_to_file(config)
+    X_data = data_util.open_data_file(read_file_path).root.data[0:8]
 
-    y_training = keras.utils.to_categorical([0], num_classes=2)
-    y_test = y_training
 
-    # CHANNEL, WIDTH, HEIGHT, DEPTH
-    backend.set_image_data_format('channels_first')
-    input_lay = Input(shape=(1, 64, 64, 64))
-    conv_1 = Conv3D(filters=3, kernel_size=(3, 3, 3), strides=(1, 1, 1))(input_lay)
-    nor_1 = BatchNormalization(axis=1)(conv_1)
-    maxp_1 = MaxPooling3D(pool_size=(2, 2, 2))(nor_1)
-    flat_1 = Flatten()(maxp_1)
+    y_data_o = data_util.open_data_file(read_file_path).root.label[0:8]
+    n_classes = np.unique(y_data_o).shape[0]
+    y_data = keras.utils.to_categorical(y_data_o, num_classes=n_classes)
+
+    input_lay = Input(shape=config['input_shape'])
+    conv_1 = Conv3D(filters=5, kernel_size=(3, 3, 3), strides=(1, 1, 1))(input_lay)
+    # nor_1 = BatchNormalization(axis=1)(conv_1)
+    maxp_1 = MaxPooling3D(pool_size=(2, 2, 2))(conv_1)
+
+    conv_2 = Conv3D(filters=8, kernel_size=(3, 3, 3), strides=(1, 1, 1))(maxp_1)
+    maxp_2 = MaxPooling3D(pool_size=(2, 2, 2))(conv_2)
+
+    conv_3 = Conv3D(filters=11, kernel_size=(3, 3, 3), strides=(1, 1, 1))(maxp_2)
+    maxp_3 = MaxPooling3D(pool_size=(2, 2, 2))(conv_3)
+
+
+    flat_1 = Flatten()(maxp_3)
     den_1 = Dense(units=1000, activation='relu')(flat_1)
     drop_1 = Dropout(rate=0.2)(den_1)
     den_2 = Dense(units=50, activation='relu')(drop_1)
-    output = Dense(units=2, activation='softmax')(den_2)
+    output = Dense(units=n_classes, activation='softmax')(den_2)
     model = Model(inputs=input_lay, outputs=output)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
-    history = model.fit(x=x_train,
-                        y=y_training,
-                        batch_size=1,
+    # print(model.summary())
+    history = model.fit(x=X_data,
+                        y=y_data,
+                        batch_size=3,
                         epochs=30,
                         verbose=1
                         )
-    score = model.predict(x_test)
+    score = model.predict(X_data)
     print(score)
--- data/data_util.py
+++ data/data_util.py
@@ -3,10 +3,13 @@ import pandas as pd
 import glob
 import os
 import numpy as np
+import tables
+
+current_path = os.path.dirname(__file__)
 
 
 def get_ids_labels(mc):
-    df = pd.read_csv('HEME-ER details for Dr Fann.csv').dropna()
+    df = pd.read_csv(os.path.join(current_path, 'HEME-ER details for Dr Fann.csv')).dropna()
     # id
     if mc == 'mri':
         ids = df[['MRI HEME #']].astype(int)
@@ -29,7 +32,6 @@ def get_ids_labels(mc):
 
 
 def get_img_paths(mc):
-    current_path = os.path.dirname(__file__)
     img_paths = ''
     if mc == 'mri':
         # to be continue...
@@ -42,15 +44,69 @@ def get_img_paths(mc):
     return img_paths
 
 
-def get_all_imgs(mc):
-    read_pathes = get_img_paths(mc)
-    a = []
-    for read_path in read_pathes:
-        sitk_t1 = sitk.ReadImage(read_path)
-        t1 = sitk.GetArrayFromImage(sitk_t1).T.reshape(-1, 1, 64, 64, 64)
-        a = np.stack(t1, axis=0)
-    print(a)
+def get_subject_id_from_path(img_path):
+    file_name = img_path.split(os.sep)[-1]
+    subject_id = file_name.split('_')[0]
+    return subject_id
+
+
+def get_subject_label(s_id, ids_labels):
+    id_label = ids_labels[ids_labels.iloc[:, 0] == int(s_id)]
+    label = id_label.iloc[:,1].values
+    return label
+
+def create_data_file(config, n_samples, file_path):
+    hdf5_file = tables.open_file(file_path, mode='w')
+    filters = tables.Filters(complevel=5, complib='blosc')
+    data_shape = tuple([0, config['n_channels']] + list(config['image_shape']))
+    try:
+        data_storage = hdf5_file.create_earray(hdf5_file.root, 'data', tables.Float32Atom(), shape=data_shape,
+                                               filters=filters, expectedrows=n_samples)
+        label_storage = hdf5_file.create_earray(hdf5_file.root, 'label', tables.Float32Atom(), shape=(0,),
+                                               filters=filters, expectedrows=n_samples)
+    except Exception as e:
+        # If something goes wrong, delete the incomplete data file
+        os.remove(file_path)
+        raise e
+    return hdf5_file, data_storage, label_storage
+
+
+def write_ct_image_data_to_file(config, img_paths, data_storage, label_storage):
+    ct_img_list = []
+    ids_labels = get_ids_labels(config['which_machine'])
+    for img_path in img_paths:
+        img = sitk.ReadImage(img_path)
+        sid = get_subject_id_from_path(img_path)
+        true_label = get_subject_label(sid, ids_labels)
+        ct_img_list.append(sitk.GetArrayFromImage(img).T)
+        data_storage.append(np.asarray(ct_img_list[:config['n_channels']])[np.newaxis])
+        label_storage.append(true_label)
+    return data_storage, label_storage
+
+
+def write_data_to_file(config):
+    file_path = os.path.join(current_path, config['which_machine'], config['which_machine'] + '_data.h5')
+    img_paths = get_img_paths(config['which_machine'])
+    n_samples = len(img_paths)
+    hdf5_file, data_storage, label_storage = create_data_file(config, n_samples, file_path)
+    if config['which_machine'] == 'ct':
+        write_ct_image_data_to_file(config, img_paths, data_storage, label_storage)
+        # write_ct_labels_to_file(config, img_paths)
+    else:
+        # MRI...to be continue...
+        pass
+
+    hdf5_file.close()
+    return file_path
+
+
+def open_data_file(filename, readwrite="r"):
+    return tables.open_file(filename, readwrite)
 
 if __name__ =='__main__':
-    get_all_imgs('ct')
+    config = dict()
+    config['image_shape'] = (64, 64, 64)
+    config['which_machine'] = 'ct'
+    config['n_channels'] = 1
+    write_data_to_file(config)
     print('done')
