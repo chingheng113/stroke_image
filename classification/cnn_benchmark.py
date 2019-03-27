0e69bcb95759941b82605d95691c155d57f8453e
chingheng113 <chingheng113@gmail.com>
2019-03-27 13:45:38 -0400

    back up

--- classification/cnn_benchmark.py
+++ classification/cnn_benchmark.py
@@ -27,32 +27,32 @@ if __name__ == '__main__':
     y_data = keras.utils.to_categorical(y_data_o, num_classes=n_classes)
 
     input_lay = Input(shape=config['input_shape'])
-    conv_1 = Conv3D(filters=5, kernel_size=(3, 3, 3), strides=(1, 1, 1))(input_lay)
+    conv_1 = Conv3D(filters=32, kernel_size=(3, 3, 3), strides=(1, 1, 1))(input_lay)
     # nor_1 = BatchNormalization(axis=1)(conv_1)
     act_1 = Activation('relu')(conv_1)
     maxp_1 = MaxPooling3D(pool_size=(2, 2, 2))(act_1)
 
-    conv_2 = Conv3D(filters=8, kernel_size=(3, 3, 3), strides=(1, 1, 1))(maxp_1)
+    conv_2 = Conv3D(filters=32, kernel_size=(3, 3, 3), strides=(1, 1, 1))(maxp_1)
     act_2 = Activation('relu')(conv_2)
     maxp_2 = MaxPooling3D(pool_size=(2, 2, 2))(act_2)
 
-    conv_3 = Conv3D(filters=11, kernel_size=(3, 3, 3), strides=(1, 1, 1))(maxp_2)
+    conv_3 = Conv3D(filters=32, kernel_size=(3, 3, 3), strides=(1, 1, 1))(maxp_2)
     act_3 = Activation('relu')(conv_3)
     maxp_3 = MaxPooling3D(pool_size=(2, 2, 2))(act_3)
 
 
     flat_1 = Flatten()(maxp_3)
-    den_1 = Dense(units=1000, activation='relu')(flat_1)
+    den_1 = Dense(units=100, activation='relu')(flat_1)
     drop_1 = Dropout(rate=0.2)(den_1)
     den_2 = Dense(units=50, activation='relu')(drop_1)
     output = Dense(units=n_classes, activation='softmax')(den_2)
     model = Model(inputs=input_lay, outputs=output)
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
     # print(model.summary())
     history = model.fit(x=X_data,
                         y=y_data,
                         batch_size=3,
-                        epochs=30,
+                        epochs=300,
                         verbose=1
                         )
     score = model.predict(X_data)
