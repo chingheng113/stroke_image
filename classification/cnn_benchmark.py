96a16f5bd94cea32aa7a40e45c3916775855ca81
chingheng113 <chingheng113@gmail.com>
2019-04-09 16:33:46 -0400

    back up

--- classification/cnn_benchmark.py
+++ classification/cnn_benchmark.py
@@ -1,6 +1,7 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('/data/linc9/stroke_image/')))
 from data import data_util
+from classification import generator
 from keras.engine import Model
 import numpy as np
 from keras.layers import Dense, Conv3D, MaxPooling3D, Flatten, Input, Activation, BatchNormalization, Dropout
@@ -11,20 +12,19 @@ backend.set_image_data_format('channels_first')
 config = dict()
 config['which_machine'] = 'ct'
 config['image_shape'] = (128, 128, 28) # CHANNEL, WIDTH, HEIGHT, DEPTH
+config['n_classes'] = 2
 if config['which_machine'] == 'ct':
     config['all_modalities'] = ['ct']
 else:
     config["all_modalities"] = ['t1', 't1ce', 'flair', 't2']
 config['n_channels'] = len(config["all_modalities"])
 config['input_shape'] = tuple([config['n_channels']] + list(config['image_shape']))
+config['batch_size'] = 10
 
 if __name__ == '__main__':
     read_file_path = data_util.write_data_to_file(config)
-    # see get_data_from_file for generator....
-    X_data = data_util.open_data_file(read_file_path).root.data[0:20]
-    y_data_o = data_util.open_data_file(read_file_path).root.label[0:20]
-    n_classes = np.unique(y_data_o).shape[0]
-    y_data = keras.utils.to_categorical(y_data_o, num_classes=n_classes)
+    data_file = data_util.open_data_file(read_file_path)
+    train_generator, validation_generator, n_train_steps, n_validation_steps = generator.get_training_and_validation_generators(data_file, config)
 
     input_lay = Input(shape=config['input_shape'])
     conv_1 = Conv3D(filters=64, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(input_lay)
@@ -48,18 +48,25 @@ if __name__ == '__main__':
     nor_4 = BatchNormalization()(den_1)
     den_2 = Dense(units=50, activation='sigmoid')(nor_4)
     nor_5 = BatchNormalization()(den_2)
-    output = Dense(units=n_classes, activation='softmax')(nor_5)
+    output = Dense(units=config['n_classes'], activation='softmax')(nor_5)
     model = Model(inputs=input_lay, outputs=output)
 
     opt = keras.optimizers.Adadelta()
     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
     # print(metricsodel.summary())
-    history = model.fit(x=X_data,
-                        y=y_data,
-                        validation_split=0.3,
-                        batch_size=3,
-                        epochs=100,
-                        verbose=1
-                        )
+    # no callback yet
+    history = model.fit_generator(generator=train_generator,
+                                  steps_per_epoch=n_train_steps,
+                                  epochs=config['batch_size'],
+                                  validation_data=validation_generator,
+                                  validation_steps=n_validation_steps,
+                                  verbose=1
+                                  )
+
+    X_data = data_util.open_data_file(read_file_path).root.data[0:20]
+    # y_data_o = data_util.open_data_file(read_file_path).root.label[0:20]
+    # n_classes = np.unique(y_data_o).shape[0]
+    # y_data = keras.utils.to_categorical(y_data_o, num_classes=n_classes)
     score = model.predict(X_data)
+
     print(score)
---
+++ classification/generator.py
@@ -0,0 +1,63 @@
+import pickle
+import numpy as np
+from random import shuffle
+import keras.optimizers
+
+
+def pickle_dump(item, out_file):
+    with open(out_file, "wb") as opened_file:
+        pickle.dump(item, opened_file)
+
+
+def split_list(input_list, validation_size=0.3, shuffle_list=True):
+    split = 1. - validation_size
+    if shuffle_list:
+        shuffle(input_list)
+    n_training = int(len(input_list) * split)
+    training = input_list[:n_training]
+    testing = input_list[n_training:]
+    return training, testing
+
+
+def get_validation_split(data_file, validation_size):
+    print("Creating validation split...")
+    nb_samples = data_file.root.data.shape[0]
+    sample_list = list(range(nb_samples))
+    training_list, validation_list = split_list(sample_list, validation_size, True)
+    # pickle_dump(training_list, 'training_ids.pkl')
+    # pickle_dump(validation_list, 'testing_ids.pkl')
+    return training_list, validation_list
+
+
+def add_data(x_list, y_list, data_file, index, config):
+    X_data = data_file.root.data[index]
+    y_data_o = data_file.root.label[index]
+    y_data = keras.utils.to_categorical(y_data_o, num_classes=config['n_classes'])
+    x_list.append(X_data)
+    y_list.append(y_data)
+
+
+def convert_data(x_list, y_list):
+    x = np.asarray(x_list)
+    y = np.asarray(y_list)
+    return x, y
+
+
+def data_generator(data_file, index_list, config):
+    while True:
+        x_list = list()
+        y_list = list()
+        shuffle(index_list)
+        while len(index_list) > 0:
+            index = index_list.pop()
+            add_data(x_list, y_list, data_file, index, config)
+            yield convert_data(x_list, y_list)
+
+
+def get_training_and_validation_generators(data_file, config):
+    training_list, validation_list = get_validation_split(data_file, validation_size=0.3)
+    train_generator = data_generator(data_file, training_list, config)
+    validation_generator = data_generator(data_file, validation_list, config)
+    n_train_steps = 3
+    n_validation_steps = 3
+    return train_generator, validation_generator, n_train_steps, n_validation_steps
