e03a05bcc7e758dbf770ee9da15ea74f9c027dac
chingheng113 <chingheng113@gmail.com>
2019-04-26 15:00:02 -0400

    back up

--- data/data_check.py
+++ data/data_check.py
@@ -7,11 +7,11 @@ sys.path.append(os.path.abspath(os.path.join('/data/linc9/stroke_image/')))
 from data import data_util
 current_path = os.path.dirname(__file__)
 
-if __name__ == '__main__':
+def check_image_shape():
     image_shape = (256, 256, 20)
     sequences = ['dwi', 'flair']
     for s in sequences:
-        print('== '+s)
+        print('== ' + s)
         mri_path = os.path.join(current_path, 'mri')
         sq_path = os.path.join(mri_path, 'n4_' + s)
         read_path = os.path.join(sq_path, '*_' + s.upper() + '.nii')
@@ -22,4 +22,25 @@ if __name__ == '__main__':
             img_data = sitk.GetArrayFromImage(img).T
             if img_data.shape != image_shape:
                 print(sid)
+
+
+def check_patient_number_cross_sequence():
+    sequences = ['dwi', 'flair']
+    id_list_sequences = []
+    for s in sequences:
+        mri_path = os.path.join('C:\\', 'Users', 'linc9', 'Desktop')
+        sq_path = os.path.join(mri_path, 'n4_' + s)
+        read_path = os.path.join(sq_path, '*_' + s.upper() + '.nii')
+        img_paths = glob.glob(read_path)
+        id_list = []
+        for img_path in img_paths:
+            sid = data_util.get_subject_id_from_path(img_path)
+            id_list.append(sid)
+        uniq_id_list = list(dict.fromkeys(id_list))
+        id_list_sequences.append(uniq_id_list)
+    print(list(set(id_list_sequences[0]) - set(id_list_sequences[1])))
+
+
+if __name__ == '__main__':
+    check_patient_number_cross_sequence()
 
