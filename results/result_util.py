34388d7fb4cfd2610887e313dd709ec5a422ee19
chingheng113 <chingheng113@gmail.com>
2019-05-02 11:42:19 -0400

    back up

--- results/result_util.py
+++ results/result_util.py
@@ -1,6 +1,17 @@
 import pickle
 import matplotlib.pyplot as plt
 from keras.models import load_model
+import keras.optimizers
+import numpy as np
+from data import data_util
+from sklearn.metrics import classification_report, confusion_matrix
+
+def labelize(y_arr):
+    y_label = []
+    for y in y_arr:
+        y_label = np.append(y_label, np.argmax(y))
+    return y_label
+
 
 def plot_training_acc(model_name):
     with open(model_name+'_trainHistory.pickle', 'rb') as f:
@@ -26,16 +37,28 @@ def plot_training_loss(model_name):
         plt.show()
 
 
-def load_model(model_name):
-    model = load_model(model_name+'.h5')
-    return model
-
-
-# def load_testing_data():
-    
+def load_testing_data():
+    config = dict()
+    config['which_machine'] = 'mri'
+    config['n_classes'] = 2
+    config["all_sequences"] = ['dwi', 'flair']
+    config['n_channels'] = len(config["all_sequences"])
+    config['image_shape'] = (20, 256, 256)
+    read_test_file_path = data_util.write_data_to_file(config, 'testing')
+    X_data = data_util.open_data_file(read_test_file_path).root.data[:]
+    y_data_o = data_util.open_data_file(read_test_file_path).root.label[:]
+    y_data = keras.utils.to_categorical(y_data_o, num_classes=config['n_classes'])
+    return X_data, y_data, y_data_o
 
 
 if __name__ == '__main__':
     model_name = 'simple_AlexNet'
-    plot_training_acc(model_name)
-    plot_training_loss(model_name)
+    # plot_training_acc(model_name)
+    # plot_training_loss(model_name)
+    model = load_model(model_name+'.h5')
+    X_data, y_data, y_data_o = load_testing_data()
+    loss, acc = model.evaluate(X_data, y_data, verbose=0)
+    predict_prob = model.predict(X_data)
+    predict_labels = labelize(predict_prob)
+    cm = confusion_matrix(y_data_o, predict_labels)
+    print(cm)
