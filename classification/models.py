46ad89ad891c0c583d766b1af1a5d57e5597c6cd
chingheng113 <chingheng113@gmail.com>
2019-05-02 15:53:00 -0400

    back up

--- classification/models.py
+++ classification/models.py
@@ -10,34 +10,34 @@ def get_AlexNet(config):
 
     input_lay = Input(shape=config['input_shape'])
     conv_1 = Conv3D(filters=96, kernel_size=(11, 11, 11), strides=(4, 4, 4), padding='same')(input_lay)
-    act_1 = Activation('relu')(conv_1)
+    act_1 = Activation('sigmoid')(conv_1)
     maxp_1 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='same')(act_1)
 
     conv_2 = Conv3D(filters=256, kernel_size=(11, 11, 11), strides=(1, 1, 1), padding='same')(maxp_1)
-    act_2 = Activation('relu')(conv_2)
+    act_2 = Activation('sigmoid')(conv_2)
     maxp_2 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='same')(act_2)
 
     conv_3 = Conv3D(filters=384, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(maxp_2)
-    act_3 = Activation('relu')(conv_3)
+    act_3 = Activation('sigmoid')(conv_3)
 
     conv_4 = Conv3D(filters=384, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(act_3)
-    act_4 = Activation('relu')(conv_4)
+    act_4 = Activation('sigmoid')(conv_4)
 
     conv_5 = Conv3D(filters=256, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(act_4)
-    act_5 = Activation('relu')(conv_5)
+    act_5 = Activation('sigmoid')(conv_5)
     maxp_5 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='same')(act_5)
 
     flat_6 = Flatten()(maxp_5)
     den_6 = Dense(units=4096)(flat_6)
-    act_6 = Activation('relu')(den_6)
+    act_6 = Activation('sigmoid')(den_6)
     drop_6 = Dropout(0.4)(act_6)
 
     den_7 = Dense(units=2048)(drop_6)
-    act_7 = Activation('relu')(den_7)
+    act_7 = Activation('sigmoid')(den_7)
     drop_7 = Dropout(0.4)(act_7)
 
     den_8 = Dense(units=1000)(drop_7)
-    act_8 = Activation('relu')(den_8)
+    act_8 = Activation('sigmoid')(den_8)
     drop_8 = Dropout(0.4)(act_8)
 
     output = Dense(units=config['n_classes'], activation='softmax')(drop_8)
