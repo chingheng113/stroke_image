f7f3a3969531c11a1d09c886702679fe6e89227e
chingheng113 <chingheng113@gmail.com>
2019-05-03 11:14:24 -0400

    back up

--- classification/models.py
+++ classification/models.py
@@ -10,41 +10,41 @@ def get_AlexNet(config):
 
     input_lay = Input(shape=config['input_shape'])
     conv_1 = Conv3D(filters=96, kernel_size=(11, 11, 11), strides=(4, 4, 4), padding='same')(input_lay)
-    act_1 = Activation('relu')(conv_1)
+    act_1 = Activation('tanh')(conv_1)
     nor_1 = BatchNormalization()(act_1)
     maxp_1 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='same')(nor_1)
 
     conv_2 = Conv3D(filters=256, kernel_size=(11, 11, 11), strides=(1, 1, 1), padding='same')(maxp_1)
-    act_2 = Activation('relu')(conv_2)
+    act_2 = Activation('tanh')(conv_2)
     nor_2 = BatchNormalization()(act_2)
     maxp_2 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='same')(nor_2)
 
     conv_3 = Conv3D(filters=384, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(maxp_2)
-    act_3 = Activation('relu')(conv_3)
+    act_3 = Activation('tanh')(conv_3)
     nor_3 = BatchNormalization()(act_3)
 
     conv_4 = Conv3D(filters=384, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(nor_3)
-    act_4 = Activation('relu')(conv_4)
+    act_4 = Activation('tanh')(conv_4)
     nor_4 = BatchNormalization()(act_4)
 
     conv_5 = Conv3D(filters=256, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(nor_4)
-    act_5 = Activation('relu')(conv_5)
+    act_5 = Activation('tanh')(conv_5)
     nor_5 = BatchNormalization()(act_5)
     maxp_5 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='same')(nor_5)
 
     flat_6 = Flatten()(maxp_5)
     den_6 = Dense(units=512)(flat_6)
-    act_6 = Activation('relu')(den_6)
+    act_6 = Activation('tanh')(den_6)
     nor_6 = BatchNormalization()(act_6)
     drop_6 = Dropout(0.4)(nor_6)
 
     den_7 = Dense(units=256)(drop_6)
-    act_7 = Activation('relu')(den_7)
+    act_7 = Activation('tanh')(den_7)
     nor_7 = BatchNormalization()(act_7)
     drop_7 = Dropout(0.4)(nor_7)
 
     den_8 = Dense(units=128)(drop_7)
-    act_8 = Activation('relu')(den_8)
+    act_8 = Activation('tanh')(den_8)
     nor_8 = BatchNormalization()(act_8)
     drop_8 = Dropout(0.4)(nor_8)
 
