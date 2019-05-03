c35ba2869365f99d61d151135c5ab76b0ff464b6
chingheng113 <chingheng113@gmail.com>
2019-05-03 15:38:53 -0400

    back up

--- classification/models.py
+++ classification/models.py
@@ -7,32 +7,32 @@ def get_AlexNet(config):
     # AlexNet
     # con dimension = floor(((n-f+2p)/s)+1)
     # pooling dimension  = floor(((n-f)/s)+1)
-
+    pad = 'valid'
     input_lay = Input(shape=config['input_shape'])
-    conv_1 = Conv3D(filters=64, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='valid')(input_lay)
+    conv_1 = Conv3D(filters=96, kernel_size=(2, 11, 11), strides=(1, 4, 4), padding=pad)(input_lay)
     act_1 = Activation('tanh')(conv_1)
     nor_1 = BatchNormalization()(act_1)
-    maxp_1 = MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1), padding='valid')(nor_1)
+    maxp_1 = MaxPooling3D(pool_size=(2, 2, 2))(nor_1)
 
-    conv_2 = Conv3D(filters=128, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='valid')(maxp_1)
+    conv_2 = Conv3D(filters=256, kernel_size=(2, 11, 11), strides=(1, 1, 1), padding=pad)(maxp_1)
     act_2 = Activation('tanh')(conv_2)
     nor_2 = BatchNormalization()(act_2)
-    maxp_2 = MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1), padding='valid')(nor_2)
+    maxp_2 = MaxPooling3D(pool_size=(2, 2, 2))(nor_2)
 
-    conv_3 = Conv3D(filters=256, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='valid')(maxp_2)
+    conv_3 = Conv3D(filters=384, kernel_size=(2, 3, 3), strides=(1, 1, 1), padding=pad)(maxp_2)
     act_3 = Activation('tanh')(conv_3)
     nor_3 = BatchNormalization()(act_3)
 
-    conv_4 = Conv3D(filters=384, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='valid')(nor_3)
+    conv_4 = Conv3D(filters=384, kernel_size=(2, 3, 3), strides=(1, 1, 1), padding=pad)(nor_3)
     act_4 = Activation('tanh')(conv_4)
     nor_4 = BatchNormalization()(act_4)
 
-    conv_5 = Conv3D(filters=384, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='valid')(nor_4)
-    act_5 = Activation('tanh')(conv_5)
-    nor_5 = BatchNormalization()(act_5)
-    maxp_5 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='valid')(nor_5)
+    # conv_5 = Conv3D(filters=256, kernel_size=(2, 3, 3), strides=(1, 1, 1), padding=pad)(nor_4)
+    # act_5 = Activation('tanh')(conv_5)
+    # nor_5 = BatchNormalization()(act_5)
+    # maxp_5 = MaxPooling3D(pool_size=(2, 2, 2))(nor_5)
 
-    flat_6 = Flatten()(maxp_5)
+    flat_6 = Flatten()(nor_4)
     den_6 = Dense(units=512)(flat_6)
     act_6 = Activation('tanh')(den_6)
     nor_6 = BatchNormalization()(act_6)
