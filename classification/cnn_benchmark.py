37af71ce0d836c4dbfb0b019c028715fb3707fa8
chingheng113 <chingheng113@gmail.com>
2019-05-16 10:44:54 -0400

    back up

--- classification/cnn_benchmark.py
+++ classification/cnn_benchmark.py
@@ -22,10 +22,10 @@ config['n_channels'] = len(config["all_sequences"])
 config['input_shape'] = tuple([config['n_channels']] + list(config['image_shape']))
 config['batch_size'] = 10
 config['n_epochs'] = 50
-config['augments'] = ['F', 'RR10', 'RL10', 'F_RR10', 'F_RL10', 'c', 'c_F', 'c_RL90', 'c_RR90'] #7397
-# config['augments'] = ['F', 'RR10', 'RL10', 'F_RR10', 'F_RL10'] #4383
-# config['augments'] = ['F', 'RR10', 'RL10', 'F_RR10', 'F_RL10', 'c', 'c_F', 'c_RL90', 'c_RR90', 'RL90', 'RR90',
-#                       'c_F_RR90', 'c_F_RL90']
+# config['augments'] = ['F', 'RR10', 'RL10', 'F_RR10', 'F_RL10', 'c', 'c_F', 'c_RL90', 'c_RR90'] #7397
+config['augments'] = ['F', 'RR10', 'RL10', 'F_RR10', 'F_RL10'] #4383
+config['augments'] = ['F', 'RR10', 'RL10', 'F_RR10', 'F_RL10', 'c', 'c_F', 'c_RL90', 'c_RR90', 'RL90', 'RR90',
+                      'c_F_RR90', 'c_F_RL90']
 
 
 if __name__ == '__main__':
