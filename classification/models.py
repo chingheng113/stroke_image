b6e95c92bd3fd964e581deac92fa4f7900b2619b
aalinc9 <chingheng113@gmail.com>
2019-05-29 09:56:12 -0400

    back up

--- classification/models.py
+++ classification/models.py
@@ -8,28 +8,28 @@ from keras.regularizers import l2
 def get_VoxCNN(config):
     model = Sequential()
     # 1st Volumetric Convolutional block
-    model.add(Convolution3D(16, (3, 3, 3), activation='relu', padding='same', input_shape=config['input_shape']))
-    model.add(Convolution3D(16, (3, 3, 3), activation='relu', padding='same'))
+    model.add(Convolution3D(8, (3, 3, 3), activation='relu', padding='same', input_shape=config['input_shape']))
+    model.add(Convolution3D(8, (3, 3, 3), activation='relu', padding='same'))
     model.add(MaxPooling3D(pool_size=(2, 2, 2)))
     # 2nd Volumetric Convolutional block
-    model.add(Convolution3D(32, (3, 3, 3), activation='relu', padding='same'))
-    model.add(Convolution3D(32, (3, 3, 3), activation='relu', padding='same'))
+    model.add(Convolution3D(16, (3, 3, 3), activation='relu', padding='same'))
+    model.add(Convolution3D(16, (3, 3, 3), activation='relu', padding='same'))
     model.add(MaxPooling3D(pool_size=(2, 2, 2)))
     # 3rd Volumetric Convolutional block
-    model.add(Convolution3D(64, (3, 3, 3), activation='relu', padding='same'))
-    model.add(Convolution3D(64, (3, 3, 3), activation='relu', padding='same'))
-    model.add(Convolution3D(64, (3, 3, 3), activation='relu', padding='same'))
+    model.add(Convolution3D(32, (3, 3, 3), activation='relu', padding='same'))
+    model.add(Convolution3D(32, (3, 3, 3), activation='relu', padding='same'))
+    model.add(Convolution3D(32, (3, 3, 3), activation='relu', padding='same'))
     model.add(MaxPooling3D(pool_size=(2, 2, 2)))
     # 4th Volumetric Convolutional block
-    model.add(Convolution3D(128, (3, 3, 3), activation='relu', padding='same'))
-    model.add(Convolution3D(128, (3, 3, 3), activation='relu', padding='same'))
-    model.add(Convolution3D(128, (3, 3, 3), activation='relu', padding='same'))
+    model.add(Convolution3D(64, (3, 3, 3), activation='relu', padding='same'))
+    model.add(Convolution3D(64, (3, 3, 3), activation='relu', padding='same'))
+    model.add(Convolution3D(64, (3, 3, 3), activation='relu', padding='same'))
     model.add(MaxPooling3D(pool_size=(2, 2, 2)))
     model.add(Flatten())
     # 1th fully connected layer with batchnorm and dropout for regularization
     model.add(Dense(128, activation='relu'))
     model.add(BatchNormalization())
-    model.add(Dropout(0.7))
+    model.add(Dropout(0.5))
     # 2th fully connected layer
     model.add(Dense(64, activation='relu'))
     # Output with softmax nonlinearity for classification
